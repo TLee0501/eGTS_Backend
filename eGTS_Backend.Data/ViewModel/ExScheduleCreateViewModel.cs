6cc16bf2b265f9b7e80544a59c79cf88e4389450
DESKTOP-M8FB9D3\Hieu Nguyen <hieunguyen3141592@gmail.com>
2023-07-11 15:58:39 +0700

    add

---
+++ eGTS_Backend.Data/ViewModel/ExScheduleCreateViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eGTS_Backend.Data.ViewModel
+{
+    public class ExScheduleCreateViewModel
+    {
+        public Guid GymerId { get; set; }
+
+        public Guid Ptid { get; set; }
+
+        public Guid PackageGymerId { get; set; }
+
+        public DateTime From { get; set; }
+
+        public DateTime To { get; set; }
+
+    }
+}
