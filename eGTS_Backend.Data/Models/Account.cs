7974036b829bdd46da8dce9424225851ec995ce8
DESKTOP-M8FB9D3\Hieu Nguyen <hieunguyen3141592@gmail.com>
2023-07-18 10:48:48 +0700

    Bug Fix In Account

--- eGTS_Backend.Data/Models/Account.cs
+++ eGTS_Backend.Data/Models/Account.cs
@@ -11,7 +11,7 @@ public partial class Account
 
     public string Password { get; set; } = null!;
 
-    public string Image { get; set; } = null!;
+    public string? Image { get; set; }
 
     public string Fullname { get; set; } = null!;
 
