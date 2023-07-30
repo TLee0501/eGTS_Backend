61e43b933ca89d8bc497178ce414da499b08792b
DESKTOP-M8FB9D3\Hieu Nguyen <hieunguyen3141592@gmail.com>
2023-07-31 00:51:15 +0700

    Package Fix

--- eGTS_Backend.Data/ViewModel/PackageViewModel.cs
+++ eGTS_Backend.Data/ViewModel/PackageViewModel.cs
@@ -12,7 +12,7 @@ namespace eGTS_Backend.Data.ViewModel
         public string Name { get; set; } = null!;
         public bool HasPt { get; set; }
         public bool HasNe { get; set; }
-        public short NumberOfsession { get; set; }
+        public short? NumberOfsession { get; set; }
         public double? Ptcost { get; set; }
         public double? Necost { get; set; }
         public double? CenterCost { get; set; }
