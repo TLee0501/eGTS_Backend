93f3735204a7b43cdb0aaaf1d7183e347603b1f8
TLee0501 <67847896+TLee0501@users.noreply.github.com>
2023-07-30 20:13:27 +0700

    Update SearchRequest

--- eGTS_Backend.Data/ViewModel/RequestViewModel.cs
+++ eGTS_Backend.Data/ViewModel/RequestViewModel.cs
@@ -10,8 +10,11 @@ namespace eGTS_Backend.Data.ViewModel
     {
         public Guid Id { get; set; }
         public Guid GymerId { get; set; }
+        public string GymerName { get; set; }
         public Guid ReceiverId { get; set; }
         public Guid PackageGymerId { get; set; }
+        public string PackageGymerName { get; set; }
+        public short NumberOfSession { get; set; }
         public bool IsPt { get; set; }
         public bool? IsAccepted { get; set; }
     }
