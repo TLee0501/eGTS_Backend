99c454dc31afc9ce258aa356676148b0e1707397
TLee0501 <67847896+TLee0501@users.noreply.github.com>
2023-09-11 09:42:38 +0700

    Update report

--- eGTS.Bussiness/ReportService/IReportService.cs
+++ eGTS.Bussiness/ReportService/IReportService.cs
@@ -1,18 +1,12 @@
 ﻿using eGTS_Backend.Data.ViewModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace eGTS.Bussiness.ReportService
 {
-    public interface IReportService 
+    public interface IReportService
     {
         Task<List<GymerPackageActiveViewModel>> GetActivePackages();
         Task<List<GymerPackageActiveViewModel>> GetDonePackages();
         Task<List<GymerPackageActiveViewModel>> GetPausePackages();
-
-        Task<ReportBasicViewModel> getBasicReportInDay();       //Chua lam
+        Task<ReportInComeViewModel> getReportInCome();
     }
 }
