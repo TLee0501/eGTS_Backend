92cbc7fabd80e8be5e9ef178f07601cf28536ba1
DESKTOP-M8FB9D3\Hieu Nguyen <hieunguyen3141592@gmail.com>
2023-09-22 09:15:40 +0700

    Revert "chinh ta"
    This reverts commit 7370db2c5aa4e7a3ced4674b7b7ae95e8bd01a92.

--- eGTS/Controllers/LoginController.cs
+++ eGTS/Controllers/LoginController.cs
@@ -65,9 +65,9 @@ namespace eGTS.Controllers
             var result = await _loginService.Login(model);
 
             if (result == null)
-                return BadRequest(new ErrorResponse(204, "Vui lòng kiểm tra lại số điện thoại/mật khẩu!"));
+                return NotFound(new ErrorResponse(204, "Vui lòng kiểm tả lại số điện thoại/mật khẩu!"));
             else if (result.IsDelete == true)
-                return Unauthorized(new ErrorResponse(401, "Tài khoản đã bị khóa!"));
+                return Unauthorized(new ErrorResponse(401, "Tài khaonr đã bị khóa!"));
 
 
             _logger.LogInformation($"Login by {model.PhoneNo}");
